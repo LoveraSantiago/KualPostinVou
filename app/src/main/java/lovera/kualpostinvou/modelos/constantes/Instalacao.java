ab01b95fa656799b517adcc32e8f73ac98846f59
Santiago Lovera <santiago.lovera@gmail.com>
2016-09-22 06:41:29 -0300

    Criando modelo para instalacao

---
+++ app/src/main/java/lovera/kualpostinvou/modelos/constantes/Instalacao.java
@@ -0,0 +1,50 @@
+package lovera.kualpostinvou.modelos.constantes;
+
+public class Instalacao {
+
+    private int codApp;
+    private int codUsuario;
+    private String dataHora;
+    private String deviceOS;
+    private String deviceToken;
+
+    public int getCodApp() {
+        return codApp;
+    }
+
+    public void setCodApp(int codApp) {
+        this.codApp = codApp;
+    }
+
+    public int getCodUsuario() {
+        return codUsuario;
+    }
+
+    public void setCodUsuario(int codUsuario) {
+        this.codUsuario = codUsuario;
+    }
+
+    public String getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(String dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public String getDeviceOS() {
+        return deviceOS;
+    }
+
+    public void setDeviceOS(String deviceOS) {
+        this.deviceOS = deviceOS;
+    }
+
+    public String getDeviceToken() {
+        return deviceToken;
+    }
+
+    public void setDeviceToken(String deviceToken) {
+        this.deviceToken = deviceToken;
+    }
+}
