314de3b2ad12265738851042ac95fa9fe3764cac
Santiago Lovera <santiago.lovera@gmail.com>
2016-10-08 10:37:32 -0300

    adcionando tratativa erro

--- app/src/main/java/lovera/kualpostinvou/views/services/ServicesNames.java
+++ app/src/main/java/lovera/kualpostinvou/views/services/ServicesNames.java
@@ -3,6 +3,7 @@ package lovera.kualpostinvou.views.services;
 public class ServicesNames {
 
     public static int NOME_GEOLOCALIZACAO = 0;
-    public static int TOKEN_APPCIVICO = 1;
-    public static int GPS_SERVICE = 2;
+    public static int TOKEN_APPCIVICO     = 1;
+    public static int GPS_SERVICE         = 2;
+    public static int SERVIDOR_OFF        = 3;
 }
